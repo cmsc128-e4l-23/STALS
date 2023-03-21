00bfa40deb058c47a37ef5053bc9ea693088ac1a
Marlo Mancenido <mumancenido@up.edu.ph>
2023-03-21 13:24:34 +0800

    Initial commit for header

---
+++ client/stals/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+function Header() {
+  return (
+    <div id='header'>
+        <div id='logo'></div>
+        <div id='search-bar'></div>
+        <div id='right-side-btns'></div>
+    </div>
+  )
+}
+
+export default Header
