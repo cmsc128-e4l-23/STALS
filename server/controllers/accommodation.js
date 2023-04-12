8885c2ece96368b7ec79fc9d81a40da5b3671091
Xeth Soto <xasoto>
2023-04-12 17:25:45 +0800

    added new callbacks functions (unarchive and viewing accommodations) and added functionalities to them and to archive accommodations.

--- server/controllers/accommodation.js
+++ server/controllers/accommodation.js
@@ -1,9 +1,43 @@
+import Accommodation from "../models/Accommodation.js";
+
 const addAccomm = async (req, res) => {
     res.send("I am adding accommodation");
 }
 
 const archiveAccomm = async (req, res) => {
-    res.send("I am archiving accommodation");
+    
+    const accomm_details = req.body;
+
+    Accommodation.updateOne(
+        { _id: accomm_details._id },
+        { $set: {archived: true} }
+        )
+        .then((result) => {
+            res.send("Successfully archived accommodation");
+        })
+        .catch((error) => {
+            console.log(err);
+
+            res.send({success: false, error: "Archive Failed"});
+        })
+}
+
+const unarchiveAccomm = async (req, res) => {
+
+    const accomm_details = req.body;
+
+    Accommodation.updateOne(
+        { _id: accomm_details._id },
+        { $set: {archived: false} }
+        )
+        .then((result) => {
+            res.send("Successfully unarchived accommodation");
+        })
+        .catch((error) => {
+            console.log(err);
+
+            res.send({success: false, error: "Unarchive Failed"});
+        })
 }
 
 const deleteAccomm = async (req, res) => {
@@ -18,10 +52,23 @@ const generateRep = async (req, res) => {
     res.send("I am generating report");
 }
 
+//for testing
+const viewAccomm = async (req, res) => {
+    Accommodation.findById('643665dccee7fa1d7dd408ea')
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+}
+
 export default {
     addAccomm,
     archiveAccomm,
+    unarchiveAccomm,
     deleteAccomm,
     searchAccomm,
-    generateRep
+    generateRep,
+    viewAccomm
 }
