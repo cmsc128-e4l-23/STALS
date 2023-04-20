74c5631b1c7b670dea45b6f02f590dd4964e8029
FarrelBeso <vabeso@up.edu.ph>
2023-04-20 16:44:43 +0800

    update: huh theres something odd

--- server/test/check.js
+++ server/test/check.js
@@ -2,7 +2,9 @@ import easytest from "./easytest.js";
 import testdata from "./testdata.js";
 
 // PRACTICALLY A PLAYGROUND FOR NEEDLE FUNCTIONS
+// necessary data here
 const usertest = testdata.additionalUsers[0];
-await easytest.registerTest(usertest);
-await easytest.loginTest(usertest);
-await easytest.loginCheckTest(usertest);
+// place your sequential callbacks here
+setTimeout(()=>easytest.registerTest(usertest),0)
+setTimeout(()=>easytest.loginTest(usertest),1000)
+setTimeout(()=>easytest.loginCheckTest(usertest),2000)
