06f124ce130d0127791e5597d5016c25acb7b269
ngarcia <ngarcia@up.edu.ph>
2023-04-22 04:25:29 +0800

    Partial implementation of functional sign up

--- client/stals/src/pages/Signup.js
+++ client/stals/src/pages/Signup.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../components/Signup.css'
 
 export default function Signup() {
+  const [usertype, setUserType]  = useState('Student');
+  const [fname, setFName]  = useState('');
+  const [lname, setLName]  = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [contact, setContact] = useState('');
+  const [sex, setSex] = useState('Male');
+  const [birthday, setBirthday] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let newUser = {
+      userType: usertype,
+      firstName: fname,
+      lastName: lname,
+      email: email,
+      password: password,
+      sex: sex,
+      phoneNumber: contact,
+      birthday: birthday
+    }
+    let response = await fetch('http://localhost:3001/signup', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newUser)
+        });
+    alert(response.status);
+  }
   return (
     <div className="signup-container">
       <h1>SIGNUP</h1>
-      <form className="signup-form">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" placeholder="Enter your name" required />
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <select id="type" name="type" value={usertype} onChange={(e) => setUserType(e.target.value)} required>
+          <option value="Student">Student</option>
+          <option value="Accommodation Owner">Accommodation Owner</option>
+        </select>
+        
+        <label htmlFor="name">First Name:</label>
+        <input type="text" id="fname" name="fname" placeholder="Enter your name" value={fname} onChange={(e) => setFName(e.target.value)} required />
+        
+        <label htmlFor="name">Last Name:</label>
+        <input type="text" id="lname" name="lname" placeholder="Enter your name" value={lname} onChange={(e) => setLName(e.target.value)} required />
+        
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="email" required />
+        <input type="email" id="email" name="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" placeholder="password" required />
+        <input type="password" id="password" name="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        
+        <label htmlFor="password">Contact Number:</label>
+        <input type="number" id="contact" name="contact" placeholder="Enter your contact number" value={contact} onChange={(e) => setContact(e.target.value)} required />
+        
+        <label htmlFor="sex">Sex:</label>
+        <select id="type" name="type" value={sex} onChange={(e) => setSex(e.target.value)} required>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+
+        <label htmlFor="password">Birthday:</label>
+        <input type="date" id="birthday" name="birthday" placeholder="Enter your birthday" value={birthday} onChange={(e) => setBirthday(e.target.value)} required />
+        
         <button type="submit">Sign Up</button>
       </form>
     </div>
