33d3c2e21842454a7294c8c2fddb33e79c7fd33b
Xeth Soto <xasoto>
2023-04-21 12:36:55 +0800

    fixed needle post tests

--- server/test/reviewTest.js
+++ server/test/reviewTest.js
@@ -1,12 +1,12 @@
 import needle from "needle";
 import mongoose from "mongoose";
 
-/* ======================= REVIEW FUNCTIONALITIES TEST ======================= */
-
-const reviewId = "6441644367c42f1489bb4129";
+// Object IDs (change this for different databases)
+const reviewId = "6442098cc9c5604bc17c078d";
 const userId = "64415c5344582ba43b014e14";
 const propertyId = "643665dccee7fa1d7dd408ea";
 
+// Review Data (for adding reviews)
 const data = {
     userId: userId,
     propertyId: propertyId,
@@ -23,7 +23,8 @@ const data2 = {
     photos: [{filename: "Insert very mid photo"}]
 }
 
-// Adding Review
+// =========================== ADDING REVIEW ===========================
+// Add Review
 // needle.post("http://localhost:3001/addReview",
 //     data,
 //     (err, res) => {
@@ -31,6 +32,7 @@ const data2 = {
 //     }
 // );
 
+// // Add Review 2
 // needle.post("http://localhost:3001/addReview",
 //     data2,
 //     (err, res) => {
@@ -38,24 +40,28 @@ const data2 = {
 //     }
 // );
 
-// Edit Review
-needle.post("http://localhost:3001/editReview",
-    {   
-        _id: reviewId,
-        content: "I hate it",
-        rating: 1,
-        photos: [{filename: "Photo2"}, {filename: "Photo3"}]
-        // photos: ["Photo2", "Photo3"]
-    },
-    (err, res) => {
-        console.log(res.body);
-    }
-)
+// =========================== EDITING REVIEW ===========================
+// // Edit Review
+// needle.post("http://localhost:3001/editReview",
+//     {   
+//         _id: reviewId,
+//         content: "I hate it",
+//         rating: 1,
+//         photos: [
+//             {filename: "Photo2"}
+//             // {filename: "Photo3"} //doesn't work
+//         ]
+//         // photos: ["Photo2", "Photo3"]
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// )
 
 // // Edit Review 2 (revert changes)
 // needle.post("http://localhost:3001/editReview",
 //     {   
-//         id: reviewId,
+//         _id: reviewId,
 //         content: "I like it",
 //         rating: 5,
 //         photos: [{filename: "Photo1"}]
@@ -65,19 +71,32 @@ needle.post("http://localhost:3001/editReview",
 //     }
 // )
 
-// Delete Review
+// =========================== DELETING REVIEW ===========================
 // needle.post("http://localhost:3001/deleteReview",
 //     {
-//         id: reviewId
+//         _id: reviewId,
+//         userId: userId
 //     },
 //     (err, res) => {
 //         console.log(res.body);
 //     }
 // )
 
+// =========================== GETTING REVIEW ===========================
+// // Get Review 1 (userID)
 // needle.post("http://localhost:3001/getReview",
 //     {
 //         userId: userId
+//     },
+//     (err, res) => {
+//         console.log(res.body);
+//     }
+// )
+
+// // Get Review 2 (propertyID)
+// needle.post("http://localhost:3001/getReview",
+//     {
+//         propertyId: propertyId
 //     },
 //     (err, res) => {
 //         console.log(res.body);
