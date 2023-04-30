90adac55344eaed339a7d6010891828ba69d9df6
FarrelBeso <vabeso@up.edu.ph>
2023-04-30 20:11:25 +0800

    update: added a version of the dump with more reviews

--- server/test/accommTest.js
+++ server/test/accommTest.js
@@ -2,7 +2,7 @@ import needle from "needle";
 import mongoose from "mongoose";
 
 // ========================== ADD ACCOMMODATION ==========================
-// const url = "http://localhost:3001/addAccomm";
+const url = "http://localhost:3001/addAccomm";
 // const newAccommodation1 = {
 //     accommodationID: new mongoose.Types.ObjectId(),
 //     name: "A Place to Stay At Home",
@@ -79,6 +79,100 @@ import mongoose from "mongoose";
 //       console.log(body);
 //     }
 //   });
+
+// fetched from test-cases branch
+const data = [
+    {
+        accommodationID: new mongoose.Types.ObjectId(),
+        name: "Nawawalang Paraiso",
+        address: {
+            postCode: "4030",
+            street: "Dos Lagos",
+            barangay: "San Fernando",
+            city: "Los Baños",
+            province: "Laguna",
+            region: "CALABARZON"
+        },
+        accommodationType: 'Transient',
+        priceRange: {
+            minPrice: 7000,
+            maxPrice: 14000
+        },
+        description: "Lost Paradise",
+        restrictions: ['No drugs allowed',],
+        reviews: [],
+    },
+    {
+        accommodationID: new mongoose.Types.ObjectId(),
+        name: "Four Sisters",
+        address: {
+            postCode: "4030",
+            street: "Magsaysay",
+            barangay: "Putimbakal",
+            city: "Los Baños",
+            province: "Laguna",
+            region: "CALABARZON"
+        },
+        accommodationType: 'Dorm',
+        priceRange: {
+            minPrice: 2400,
+            maxPrice: 3700
+        },
+        description: "Affordable dorms for students",
+        restrictions: ['No cooking allowed inside',],
+        reviews: [],
+    },
+    {
+        accommodationID: new mongoose.Types.ObjectId(),
+        name: "Mayfair Apartments",
+        address: {
+            postCode: "1234",
+            street: "Apolinario",
+            barangay: "Mabini",
+            city: "Quezon",
+            province: "NCR",
+            region: "NCR"
+        },
+        accommodationType: 'Dorm',
+        priceRange: {
+            minPrice: 5000,
+            maxPrice: 7000
+        },
+        description: "Transient, just like home!",
+        restrictions: ['Help thy landlord harvest wheat for thou art a peasant',],
+        reviews: [],
+    },
+    {
+        accommodationID: new mongoose.Types.ObjectId(),
+        name: "100% very VERY GOOD rentspACe NO CAP frfr :OOOOO!!1!!",
+        address: {
+            postCode: "6666",
+            street: "Maligaya",
+            barangay: "HappyLand",
+            city: "Makati",
+            province: "NCR",
+            region: "NCR"
+        },
+        accommodationType: 'Rent',
+        priceRange: {
+            minPrice: 0,
+            maxPrice: 100
+        },
+        description: "Definitely safe :)",
+        restrictions: ['Nothing... just nothing :)))',],
+        reviews: [],
+    },
+]
+for (let d of data) {
+    needle.post(url, d, { json: true }, (err, res, body) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(body);
+    }
+  });
+}
+
 // ========================== ARCHIVE ACCOMMODATION ==========================
 // needle.post("http://localhost:3001/archiveAccomm",
 //     {
