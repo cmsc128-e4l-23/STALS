65177e27ab650f8dfd4e623878aa5b7ba30cffb5
ngarcia <ngarcia@up.edu.ph>
2023-04-30 21:42:25 +0800

    initial setup

---
+++ client/stals/src/pages/Accommodation-List/List.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+
+function fetchAccomms(email){
+    
+}
+
+
+
+export default function List({email}){
+
+    return(
+        <>
+        {email}
+        </>
+    )
+}
