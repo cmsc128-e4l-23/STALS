759260d4351a98c383c17cdefc8eca4fbbd08b04
FarrelBeso <vabeso@up.edu.ph>
2023-04-30 11:22:50 +0800

    update: coded the add review fix

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -13,6 +13,12 @@ const addReview = async (req, res) => {
         const content = review_details.content;
         const rating = review_details.rating;
         const photos = review_details.photos;
+
+        // additional checking
+        // I am not sure whether rating is a float or integer
+        if (rating < 0 || rating > 5) throw new Error("Invalid Rating Range");
+        // let's assume that content length shouldn't be that long
+        if (content.length > 2000) throw new Error("Content shouldn't exceed 2000 characters")
         
         //Creating new review
         const newReview = new Review({
@@ -24,15 +30,20 @@ const addReview = async (req, res) => {
         });
         const savedReview = await newReview.save();
 
-        //Adding the newly created review to current user
+        //Adding the newly created review to current user and to the accommodation
         const user = await User.findById(userId);
-        if (user){
+        const accomm = await Accommodation.findById(propertyId);
+        
+        if (user && accomm){
             user.reviews.push(savedReview._id);
+            accomm.reviews.push(savedReview._id);
             await user.save();
+            await accomm.save();
 
             res.send({ success: true, message: "Successfully added new review" });
         } else {
-            throw new Error("User not found");
+            if (!user) throw new Error("User not found");
+            if (!accomm) throw new Error("Accommodation not found");
         }
         
     } catch (err){
