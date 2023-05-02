8930b6c9d6b3cd2735b09e9eec5d504d9093a9c2
mauimaui <101412933+mauimaui17@users.noreply.github.com>
2023-05-02 21:17:31 +0800

    Image Test
    changed dirname for testing, image used for testing included.

---
+++ server/test/imageTest.js
@@ -0,0 +1,27 @@
+import needle from "needle";
+import mongoose from "mongoose";
+import path from "path";
+import fs from "fs";
+const userId = "64415c5344582ba43b014e14";
+const propertyId = "643665dccee7fa1d7dd408ea";
+const url = "http://loaclhost:3001/uploadImage";
+const dirname = path.resolve();
+const imgPath = path.join(dirname,'test.png');
+const testImg = {
+    imgID: new mongoose.Types.ObjectId(),
+    userID: userId,
+    accommodationID: propertyId,
+    image: {
+        file: fs.readFileSync(imgPath),
+        content_type: 'image/png'
+    }
+};
+
+needle.post(url, testImg, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(res.body);
+    console.log("Image uploaded");
+  }
+});
