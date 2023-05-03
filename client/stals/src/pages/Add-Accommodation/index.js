58ec8e64128c31d857c2d3f2b7026be9fc751614
inno29 <125535109+inno29@users.noreply.github.com>
2023-05-03 12:12:16 +0800

    fixed the button layouts and spacing

--- client/stals/src/pages/Add-Accommodation/index.js
+++ client/stals/src/pages/Add-Accommodation/index.js
@@ -1,106 +1,167 @@
 import "./Accommodation-form.css";
-import BasicInfo from "./1_BasicInfo";
 import React, { useState, useEffect } from "react";
+import Header from "components/Header";
+import BasicInfo from "./1_BasicInfo";
 import AccommInfo from "./2_AccommInfo";
 import OtherInfo from "./3_OtherInfo";
 import { useNavigate } from "react-router-dom";
 
+export default function AddAccommodation() {
+  let navigate = useNavigate();
 
-export default function AddAccommodation(){
-    let navigate = useNavigate();
+  const [page, setPage] = useState(0);
+  const [formData, setFormData] = useState({
+    name: "",
+    owner: localStorage.getItem('email'),
+    landmarks: [],
+    address: {
+        postCode: "",
+        street: "",
+        barangay: "",
+        city: "",
+        province: "Laguna",
+        region: "CALABARZON",
+    },
+    generalLocation: 0,
+    accommodationType: "Transient",
+    amenities: [],
+    priceRange: {
+        minPrice: 0,
+        maxPrice: 0
+    },
+    description: "",
+    photos: [],
+    restrictions: [],
+    security: "",
+    archived: true,
+  });
 
-    const [page, setPage] = useState(0);
-    const [formData, setFormData] = useState({
-        name: "",
-        landmarks: [],
-        address: {
-            postCode: "",
-            street: "",
-            barangay: "",
-            city: "",
-            province: "Laguna",
-            region: "CALABARZON",
-        },
-        generalLocation: 0,
-        accommodationType: "Transient",
-        amenities: [],
-        priceRange: 0,
-        description: "",
-        photos: [],
-        restrictions: [],
-        security: "",
-        archived: true,
-      });
+  const submit = () => {
+    fetch("http://localhost:3001/addAccomm", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    }).then((res) => {
+      if (res.status === 201) {
+        alert("Successfully signed up " + formData.accommodationType);
+      }
+    });
+  };
 
-    const submit = () => {
-        fetch('http://localhost:3001/addAccomm', {
-        method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData)
-        })
-        .then(res => {
-            if(res.status === 201){
-              alert("Successfully signed up " + formData.accommodationType);
-            }
-          });
+  const FormTitles = [
+    "Basic Information",
+    "Detailed Information",
+    "Other Information",
+  ];
+
+  const RenderPage = () => {
+    switch (page) {
+      case 0:
+        return <BasicInfo formData={formData} setFormData={setFormData} />;
+      case 1:
+        return <AccommInfo formData={formData} setFormData={setFormData} />;
+      case 2:
+        return (
+          <div>
+            <OtherInfo formData={formData} setFormData={setFormData} />
+          </div>
+        );
+      default:
+        return <div></div>;
     }
+  };
 
-    const FormTitles = ["Basic Information", "Detailed Information", "Other Information"];
-
-    const RenderPage = () => {
-        switch(page){
-            case(0):
-                return <BasicInfo formData={formData} setFormData={setFormData} />
-            case(1):
-                return <AccommInfo formData={formData} setFormData={setFormData} />
-            case(2):
-                return <div><OtherInfo formData={formData} setFormData={setFormData} /><button onClick={submit}>Add Accommodation</button></div>
-            default:
-                return <div></div>
+  useEffect(() => {
+    fetch("http://localhost:3001/checkifloggedin", {
+      method: "POST",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (
+          data.isLoggedIn &&
+          localStorage.getItem("usertype") === "Accommodation Owner"
+        ) {
+        } else {
+          alert("You are not an accommodation owner");
+          navigate("/home");
         }
-    }
+      });
+  }, [navigate]);
 
-    useEffect(() => {
-        fetch('http://localhost:3001/checkifloggedin', {
-        method: 'POST',
-        credentials: 'include'
-        })
-        .then(res => res.json())
-        .then(data => {
-            if(data.isLoggedIn && localStorage.getItem('usertype') === 'Accommodation Owner'){
-            }else{
-                alert('You are not an accommodation owner');
-                navigate('/home');
-            }
-        })
-    }, [navigate]);
-
-    return(
-        <>
-        <div className="form">
-            <div className="progress-bar"></div>
-            <div className="form-container">
-                <div className="form-header">
-                    <h1>{FormTitles[page]}</h1>
-                </div>
-                <div className="form-body">{RenderPage()}</div>
-                <div className="form-footer">
-                    <button
-                        disabled={page === 0}
-                        onClick={() => {
-                            setPage((currPage) => currPage - 1);
-                        }}
-                    >Prev</button>
-                    <button
-                        disabled={page === FormTitles.length - 1}
-                        onClick={() => {
-                            
-                            setPage((currPage) => currPage + 1);
-                        }}
-                    >Next</button>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="page-header">
+        <Header />
+      </div>
+      <div
+        className="form"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "100px",
+        }}
+      >
+        <div className="progress-bar">
         </div>
-        </>
-    );
+        <div
+          className="form-container"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <div className="form-header">
+            <h1>{FormTitles[page]}</h1>
+          </div>
+          <div className="form-body">{RenderPage()}</div>
+          <div className="form-footer" style={{alignItems:"center", display:"grid"}}>
+          {page === FormTitles.length - 1 &&
+          <tr>
+  <button
+    style={{
+    marginBottom:"100px",
+      width: "180px",
+      backgroundColor: "#0a4424",
+      color: "white",
+      borderRadius: "5px",
+      fontSize: "larger",
+      padding: "5px",
+      cursor: "pointer",
+    }}
+    onMouseOver={(e) => {
+      e.target.style.backgroundColor = "green";
+    }}
+    onMouseOut={(e) => (e.target.style.backgroundColor = "#0a4424")}
+    onClick={submit}
+  >
+    Add Accommodation
+  </button>
+</tr>
+
+          	
+          }
+          <tr style={{ justifyContent: "center", position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, 700%)" }}>
+            <button
+              style={{gridRow:"2", justifySelf:"center",border:"1px solid maroon", width:"100px", height:"50px", borderRadius:"5px", fontSize:"larger", cursor:"pointer", marginRight:"20px"}}
+              disabled={page === 0}
+              onClick={() => {
+                setPage((currPage) => currPage - 1);
+              }}
+            >
+              Prev
+            </button>
+            <button
+              style={{gridRow:"2", justifySelf:"center", border:"1px solid maroon", width:"100px", height:"50px", borderRadius:"5px", fontSize:"larger", cursor:"pointer"}}
+              disabled={page === FormTitles.length - 1}
+              onClick={() => {
+                setPage((currPage) => currPage + 1);
+              }}
+            >
+              Next
+            </button>
+            </tr>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
