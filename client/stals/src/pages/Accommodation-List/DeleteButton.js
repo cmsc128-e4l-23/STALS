bb2805689d0d36afe69c4648b4dc812d5b718df2
Marlo Mancenido <mumancenido@up.edu.ph>
2023-05-08 11:35:49 +0800

    Rebased with main branch and merged button implementations.

--- client/stals/src/pages/Accommodation-List/DeleteButton.js
+++ client/stals/src/pages/Accommodation-List/DeleteButton.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import DeleteModal from './DeleteModal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import "./Button.css"
+
+
 
 export default function DeleteButton({ accommodation, setLoading }){
     const [modalOpen, setModalOpen] = useState(false);
@@ -7,8 +12,11 @@ export default function DeleteButton({ accommodation, setLoading }){
 
     return(
         <>
-        <button onClick={() => setModalOpen(true)}>
+        <button className='delete-btn' onClick={() => setModalOpen(true)}>
             Delete
+            <span style={iconStyle}>
+                <FontAwesomeIcon icon={faTrash}/>
+            </span>
         </button>
 
         {modalOpen && <DeleteModal setModalOpen={setModalOpen} setLoading={setLoading} accommodation={accommodation} />}
--- client/stals/src/pages/Accommodation-List/UnarchiveButton.js
+++ client/stals/src/pages/Accommodation-List/UnarchiveButton.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import UnarchiveModal from './UnarchiveModal.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArchive } from '@fortawesome/free-solid-svg-icons'
+import "./Button.css"
 
 export default function UnarchiveButton({ accommodation, setLoading }){
     const [modalOpen, setModalOpen] = useState(false);
 
     return(
         <>
-        <button onClick={() => setModalOpen(true)}>
+        <button className='unarchive-btn' onClick={() => setModalOpen(true)}>
             Unarchive
+            <span style={iconStyle}>
+                <FontAwesomeIcon icon={faArchive}/>
+            </span>
         </button>
 
         {modalOpen && <UnarchiveModal setModalOpen={setModalOpen} setLoading={setLoading} accommodation={accommodation} />}
