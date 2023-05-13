0628b6754f6824ef8beb7121ccd92401f561082c
jfeching <jeching@up.edu.ph>
2023-05-13 18:49:08 +0800

    refactored review func testing

--- server/__test__/review.test.js
+++ server/__test__/review.test.js
@@ -42,131 +42,116 @@ const accomm_details = {
     security: "Safe and secure"
 };
 
+let accomm;
 
+let review_details = {
+    user: "mtate@gmail.com",
+    propertyId: "",
+    content: "I like it",
+    rating: 1,
+    photos: [{ filename: "Photo1" }]
+}
 describe("POST /addReview", () => {
     test("With complete and correct parameters", async () => {
         await request(app).post("/signup").send(signup_details);
         await request(app).post("/addAccomm").send(accomm_details);
-        const accomm = await Accommodation.findOne({ name: accomm_details.name })
-        var review_details = {
-            user: "mtate@gmail.com",
-            propertyId: accomm._id,
-            content: "I like it",
-            rating: 1,
-            photos: [{ filename: "Photo1" }]
-        }
+        accomm = await Accommodation.findOne({ name: accomm_details.name });
+        review_details.propertyId = accomm._id;
 
         const res = await request(app).post("/addReview").send(review_details);
         console.log(res.body);
         expect(res.body.success).toBe(true);
     });
-});
-
-
-describe("add new review with incorrect user cred", () => {
-    test("should not add a new review", async () => {
-        const res = await request(app).post("/addReview")
-            .send({
-                user: "mtatea@gmail.com",
-                propertyId: "644e58b2f157a1f22a80e741",
-                content: "I like it",
-                rating: 1,
-                photos: [{ filename: "Photo1" }]
-            },);
+    test("Adding new review with incorrest user credential", async () => {
+        var temp = review_details.user;
+        review_details.user = "mtatea@gmail.com";
+        const res = await request(app).post("/addReview").send(review_details);
+        review_details.user = temp;
         console.log(res.body);
         expect(res.body.success).toBe(false);
-        expect(res.body.error).toBe("User not found!");
+        expect(res.body.error).toBe("User not found");
     });
 });
 
-//change id of review for edit
+var review_edit = {
+    _id: "",
+    content: "I like it vvm",
+    rating: 4,
+    photos: [{ filename: "Photo1" }]
+}
 
-describe("edit existing review", () => {
-    test("should edit review", async () => {
+describe("POST /editReview", () => {
+    test("Edit an existing review with correct parameters", async () => {
         const review = await Review.findOne({})
-        const reviewId = review._id.toString()
-        const res = await request(app).post("/editReview")
-            .send({
-                _id: reviewId,
-                content: "I like it vvm",
-                rating: 4,
-                photos: [{ filename: "Photo1" }]
-            },);
+        review_edit._id = review._id.toString();
+        const res = await request(app).post("/editReview").send(review_edit);
         console.log(res.body);
         expect(res.body.success).toBe(true);
     });
-});
-let reviewId;
-// //change id of review for deletion
-describe("delete review with wrong user cred", () => {
-    test("deleting a review with wrong user email", async () => {
-        const review = await Review.findOne({})
-        const reviewId = review._id.toString()
-        const res = await request(app).post("/deleteReview")
-            .send({
-                _id: reviewId,
-                user: "mtaae@gmail.com",
-                propertyId: "644e58b2f157a1f22a80e741"
-            },);
-        console.log(res.body);
-        expect(res.body.success).toBe(false);
-        expect(res.body.error).toBe("Accomodation/User not found");
-    });
-    test("should delete review", async () => {
-        const review = await Review.findOne({})
-        reviewId = review._id.toString()
-        const accomm = await Accommodation.findOne({ name: accomm_details.name })
-        const res = await request(app).post("/deleteReview")
-            .send({
-                _id: reviewId,
-                user: "mtate@gmail.com",
-                propertyId: accomm._id
-            },);
-        console.log(res.body);
-        expect(res.body.success).toBe(true);
-    });
-});
-
-
-describe("delete review again", () => {
-    test("deleting a deleted review", async () => {
-
-        const accomm = await Accommodation.findOne({ name: accomm_details.name })
-        const res = await request(app).post("/deleteReview")
-            .send({
-                _id: reviewId,
-                user: "mtate@gmail.com",
-                propertyId: accomm._id
-            },);
+    test("Edit an existing review with wrong review_id", async () => {
+        review_edit._id = "644e58b2f157a1f22a80e741";
+        const res = await request(app).post("/editReview").send(review_edit);
         console.log(res.body);
         expect(res.body.success).toBe(false);
         expect(res.body.error).toBe("Review not found");
     });
 });
 
-// //change id of review for fetch
-const accommId = "644e58b2f157a1f22a80e741"
-describe("fetch all reviews of accomm", () => {
-    test("should retrieve all reviews from an accomm", async () => {
-
-        const res = await request(app).post("/getReview")
-            .send({
-                propertyId: accommId
-            },);
+var review_delete = {
+    _id: "",
+    user: "mtaae@gmail.com",
+    propertyId: ""
+};
+// //change id of review for deletion
+describe("POST /deleteReview", () => {
+    test("Deleting a review with wrong user email", async () => {
+        const review = await Review.findOne({})
+        review_delete._id = review._id.toString()
+        review_delete.propertyId = accomm._id
+        const res = await request(app).post("/deleteReview")
+            .send(review_delete);
+        console.log(res.body);
+        expect(res.body.success).toBe(false);
+        expect(res.body.error).toBe("Accomodation/User not found");
+    });
+    test("Deleting a review with correct parameters", async () => {
+        review_delete.user = signup_details.email;
+        const res = await request(app).post("/deleteReview")
+            .send(review_delete);
         console.log(res.body);
         expect(res.body.success).toBe(true);
     });
+    test("Deleting an already deleted review", async () => {
+        const res = await request(app).post("/deleteReview")
+            .send(review_delete);
+        console.log(res.body);
+        expect(res.body.success).toBe(false);
+        expect(res.body.error).toBe("Review not found");
+    });
 });
 
-describe("fetch all reviews of a user", () => {
-    test("should retrieve all reviews of an accomm", async () => {
-
+describe("POST /getReview", () => {
+    test("Getting all reviews on an accomm", async () => {
+        await request(app).post("/addReview").send(review_details); //adding a review again
         const res = await request(app).post("/getReview")
-            .send({
-                user: "mtate@gmail.com"
-            },);
+            .send({ propertyId: accomm._id });
         console.log(res.body);
         expect(res.body.success).toBe(true);
+    });
+    test("Getting all reviews of a user", async () => {
+
+        const res = await request(app).post("/getReview")
+            .send({ user: signup_details.email });
+        console.log(res.body);
+        expect(res.body.success).toBe(true);
+    });
+    test("Getting all reviews of an unknown user", async () => {
+
+        const res = await request(app).post("/getReview")
+            .send({ user: "unknown@gmail.com" });
+        console.log(res.body);
+        expect(res.body.success).toBe(false);
+        expect(res.body.error).toBe("User not found");
     });
 });
 
