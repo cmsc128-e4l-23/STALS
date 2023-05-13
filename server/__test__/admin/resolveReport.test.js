48f096c86765dea7f54e2b1334b45e94decdbf9e
amtuazon4 <amtuazon4@up.edu.ph>
2023-05-13 20:23:39 +0800

    initial setup for resolveReport Test Cases

--- server/__test__/admin/resolveReport.test.js
+++ server/__test__/admin/resolveReport.test.js
@@ -1,20 +1,29 @@
-import request from "supertest";
-const baseURL = "http://localhost:3001";
+import app from '../app';
+import makeDB from '../mongoose';
+import mongoose from 'mongoose';
+import request from 'supertest';
+beforeAll(() => makeDB('mongodb://0.0.0.0:27017/STALS_TEST'))
 
-//Provide an id of a pending report in database to utilize the resolveReport
-// const pendingId = "64590ef70727f84cb1e8a472"
 
 
-describe("/resolveReport Test", () =>{
+describe("POST /resolveReport", () =>{
     // test("should return the response for a successful resolveReport", async ()=>{
     //     const res = await request(baseURL).post("/resolveReport").send({_id: pendingId});
     //     expect(res.body.success).toBe(true); 
     //     expect(res.body.msg).toBe("Resolving succeeded"); 
     // });
 
-    test("should return the response for unsuccessful resolve", async ()=>{
-        const res = await request(baseURL).post("/resolveReport").send({_id: "pendingId"});
-        expect(res.body.success).toBe(false); 
-        expect(res.body.msg).toBe("Resolving failed"); 
-    });
+    // test("should return the response for unsuccessful resolve", async ()=>{
+    //     const res = await request(baseURL).post("/resolveReport").send({_id: "pendingId"});
+    //     expect(res.body.success).toBe(false); 
+    //     expect(res.body.msg).toBe("Resolving failed"); 
+    // });
+
+    
+})
+
+
+afterAll(() => {
+    mongoose.connection.db.dropDatabase()
+    .then(() => mongoose.connection.close())
 })
