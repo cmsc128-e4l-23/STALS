137566150f0b68d658903c282cde8f98719a79d4
FarrelBeso <vabeso@up.edu.ph>
2023-05-14 11:10:21 +0800

    update: add of simple tests

--- server/__test__/accommodation.test.js
+++ server/__test__/accommodation.test.js
@@ -209,6 +209,7 @@ describe("POST /deleteAccomm", () => {
             const result1 = await request(app).post("/editAccomm").send({_id: newAccomm._id, owner: "615ab89dcf32a1a2345abcde"});
             newAccomm = await Accommodation.findOne({name: mockAccomm.name})
             
+
             const res = await request(app).post("/deleteAccomm").send({_id: newAccomm._id});
             expect(res.body.success).toBe(false);
             expect(res.body.msg).toBe("Unsuccessfully deleted accommodation");
@@ -221,3 +222,44 @@ afterAll(() => {
     mongoose.connection.db.dropDatabase()
     .then(() => mongoose.connection.close())
 })
+
+const url = "http://localhost:3001";
+describe("Searching Test", () => {
+    test("Should return accomms that has 'Laguna' as province", async () => {
+        const res = await request(url).post("/searchAccomm")
+            .send({
+               searchString: "Laguna"
+            },);
+        expect(res.body.success).toBe(true);
+        const expectids = [
+            '644cdb964c5e0d977fa685ac',
+            '644cdb964c5e0d977fa685af',
+            '644cdb964c5e0d977fa685b2',
+            '644ce1aad31be0a75c33df74',
+            '644e58b2f157a1f22a80e73b',
+            '644e58b2f157a1f22a80e738'
+        ]
+        res.body.result.forEach(element => {
+            expect(expectids).toContain(element._id)
+        })
+    });
+
+    test("Should return accomms that has 'Laguna' as province even with different casings", async () => {
+        const res = await request(url).post("/searchAccomm")
+            .send({
+               searchString: "lAguNA"
+            },);
+        expect(res.body.success).toBe(true);
+        const expectids = [
+            '644cdb964c5e0d977fa685ac',
+            '644cdb964c5e0d977fa685af',
+            '644cdb964c5e0d977fa685b2',
+            '644ce1aad31be0a75c33df74',
+            '644e58b2f157a1f22a80e73b',
+            '644e58b2f157a1f22a80e738'
+        ]
+        res.body.result.forEach(element => {
+            expect(expectids).toContain(element._id)
+        })
+    });
+});
