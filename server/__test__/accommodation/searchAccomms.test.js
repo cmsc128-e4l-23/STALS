699d7c463dbc5f49c63aa5cce4eca3671ee4fed4
FarrelBeso <vabeso@up.edu.ph>
2023-05-16 14:18:33 +0800

    update: search accomms now ok

--- server/__test__/accommodation/searchAccomms.test.js
+++ server/__test__/accommodation/searchAccomms.test.js
@@ -1,60 +1,26 @@
-import app from "../../app";
 import request from "supertest";
 import makeDB from "../../mongoose";
+import mongoose from "mongoose";
 
-// simplified data
-const laguna_names = ['White House', 'Big Bellys']
-const manila_names = ['Luxury Condo', 'Cozy Apartment']
-const mock_accomms = [
-    {
-        name: "White House",
-        address: {
-            province: "Laguna",
-        },
-        amenities: "Free Wi-Fi",
-        description: "Presidential suite",
-        priceRange: { minPrice: 5000, maxPrice: 10000}
-    },
-    {
-        name: "Big Bellys",
-        address: {
-            province: "Laguna",
-        },
-        amenities: "Free Wi-Fi",
-        description: "Presidential suite",
-        priceRange: { minPrice: 5000, maxPrice: 10000}
-    },
-    {
-        name: "Luxury Condo",
-        address: {
-            province: "Metro Manila",
-        },
-        amenities: "Free Wi-Fi",
-        description: "Presidential suite",
-        priceRange: { minPrice: 5000, maxPrice: 10000}
-    },
-    {
-        name: "Cozy Apartment",
-        address: {
-            province: "Metro Manila",
-        },
-        amenities: "Free Wi-Fi",
-        description: "Presidential suite",
-        priceRange: { minPrice: 5000, maxPrice: 10000}
-    }
-]
+beforeAll(() => makeDB('mongodb://0.0.0.0:27017/STALS_TEST'));
 
 const url = "http://localhost:3001";
 
-beforeAll(async () => {makeDB('mongodb://0.0.0.0:27017/STALS_TEST')});
+// simplified data
+const laguna_names = [
+    'White House',
+    'Big Bellys',
+    'F Park',
+    'Beautiful Villa',
+    'Four Sisters',
+    'Nawawalang Paraiso'
+]
+const manila_names = [
+    'Luxury Condo',
+    'Cozy Apartment'
+]
 
 describe("Searching Test", () => {
-
-    it("Data Initialization", async() => {
-        for (let accomm of mock_accomms) {
-            (await request(app).post("/addAccomm")).send(accomm)
-        }
-    })
 
     test("Should return accomms that has 'Laguna' as province", async () => {
         const res = await request(url).post("/searchAccomm")
@@ -80,17 +46,17 @@ describe("Searching Test", () => {
         })
     });
 
-    test("Should return accomms that has 'Laguna' as province and only returns one result", async () => {
+    test("Should return accomms that has 'Laguna' as province and only returns three results", async () => {
         const res = await request(url).post("/searchAccomm")
             .send({
                 searchString: "Laguna",
-                returnLength: 1,
+                returnLength: 3,
             },);
         expect(res.body.success).toBe(true);
         res.body.result.forEach(element => {
             expect(laguna_names).toContain(element.name)
         })
-        expect(res.body.result.length).toBe(1);
+        expect(res.body.result.length).toBe(3);
     });
 
     test("Should return accomms that has 'Laguna' as province even with different casings", async () => {
@@ -145,3 +111,8 @@ describe("Searching Test", () => {
         expect(res.body.result.length).toEqual(0);
     });
 });
+
+afterAll(() => {
+    mongoose.connection.db.dropDatabase()
+    .then(() => mongoose.connection.close())
+})
