636695c0cde3b5df60b25b4683c5a2476fff61e9
mauimaui <101412933+mauimaui17@users.noreply.github.com>
2023-05-16 21:56:16 +0800

    printing stuff

--- server/__test__/image/imageUpload.test.js
+++ server/__test__/image/imageUpload.test.js
@@ -4,9 +4,10 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import fs from "fs";
 import path from 'path';
+import Image from '../../models/Image';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
+//remember to create an empty "uploads" folder in the server.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 beforeAll(() => makeDB('mongodb://0.0.0.0:27017/STALS_TEST'))
@@ -16,17 +17,18 @@ describe('POST /uploadImage', () =>{
     const propertyId = "643665dccee7fa1d7dd408ea";
     const imgPath = path.resolve(__dirname, "./noot.png");
     const imgPath2 = path.resolve(__dirname, "./sumeru.jpg");
-    const imgPath3 = path.resolve(__dirname, "./ha.jpg")
+    const imgPath3 = path.resolve(__dirname, "./ha.jpg");
     test("Successfully upload 1 image to database.", async () => {
         const response = await request(app)
             .post("/uploadImage")
             .field('userId', userId)
             .field('attachedTo', propertyId)
-            //replace 'images' with whatever is in the frontend form's name/id. for example, 'upload-images'.
             .attach('images',imgPath, 'image1.jpg');
         console.log(response.body);
         expect(response.body.success).toBe(true)
         expect(response.body.msg).toBe("Succesfully stored image to database")
+        // const result =  await Image.find({});
+        // console.log(result);
     })
     test("Successfully upload a lot of images to database.", async () => {
         const response = await request(app)
