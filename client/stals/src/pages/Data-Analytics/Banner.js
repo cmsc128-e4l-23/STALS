7daa9c9cc45632ea2ff06d842ff700b3ada7202e
colette-exe <auadoptante@up.edu.ph>
2023-05-20 13:47:57 +0800

    add: Banner.js

--- client/stals/src/pages/Data-Analytics/Banner.js
+++ client/stals/src/pages/Data-Analytics/Banner.js
@@ -1,11 +1,58 @@
-import React from "react";
+import { React, useState, useEffect, useCallback } from "react";
 import './Analytics.css';
+import { CircularProgress } from "@mui/material";
 
 export default function AnalyticsBanner() {
+    const [data, setData] = useState(null);
+    const [siteTraffic, setSiteTraffic] = useState(null);
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const day = today.getDate();
+
+    const getVisits = () => {
+        fetch('http://localhost:3001/getVisits', {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify({
+                year: year,
+                month: month,
+                day: day
+            }),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(body => {
+                if (body.success) {
+                    setSiteTraffic(body.return);
+                    console.log(body);
+                }
+        })
+    }
+
+    // get site traffic
+    useEffect(() => {
+        fetch('http://localhost:3001/dataAnalytics', {
+            method: 'GET',
+            credentials: 'include'
+        })
+            .then(res => res.json())
+            .then(body => {
+                if (body.success) {
+                    setData(body.return);
+                    console.log(body);
+                    getVisits();
+                    console.log(siteTraffic);
+            }
+        })
+    }, []);
+
     return (
         <div id="data-alaytics-banner">
             <div className="container">
-                <h2>Site Traffic</h2>
+                {siteTraffic == null ? <CircularProgress /> : <h2>Site Traffic: { siteTraffic.numVisits }</h2>}
             </div>
             <div className="container">
                 <h2>A</h2>
