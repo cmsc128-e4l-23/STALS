c0dcc7997fef72c1b240e0babb33746413d121eb
ngarcia <ngarcia@up.edu.ph>
2023-05-21 22:35:28 +0800

    added error notification

--- client/stals/src/pages/Login/index.js
+++ client/stals/src/pages/Login/index.js
@@ -54,6 +54,8 @@ export default function Login() {
         localStorage.setItem("usertype", data.type)
         
         navigate('/home');
+      }else{
+        alert(data.error);
       }
     });
   }
