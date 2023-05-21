f99e03a747e1bd3a4deafbe2bafda68879bc0246
inno29 <ivlisboa@up.edu.ph>
2023-05-21 13:49:05 +0000

    profile page

--- client/stals/src/App.js
+++ client/stals/src/App.js
@@ -6,6 +6,7 @@ import AddAccommodation from'pages/Add-Accommodation/index';
 import AccommodationList from 'pages/Accommodation-List/index';
 import AccommPage from 'pages/Accommodation-Page/index';
 import AdminPage from 'pages/Admin';
+import Profile from './pages/Profile/index';
 
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
@@ -21,8 +22,9 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/add-accommodation" element={<AddAccommodation  />} />
           <Route path="/your-accommodations" element={<AccommodationList />} />
-          <Route path="/accomm" element={<AccommPage />} />
+          <Route path="/accommodation-page" element={<AccommPage />} />
           <Route path="/admin" element={<AdminPage />} />
+          <Route path="/profile-page" element={<Profile/>} />
         </Routes> 
       </Router>
     </div>
