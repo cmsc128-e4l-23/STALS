1d57b3666b890c9227084edcab10ebd96f58592e
colette-exe <auadoptante@up.edu.ph>
2023-05-24 00:07:34 +0800

    feat: increment number of visits

--- client/stals/src/App.js
+++ client/stals/src/App.js
@@ -8,10 +8,43 @@ import AdminPage from 'pages/Admin';
 import DataAnalytics from 'pages/Data-Analytics';
 
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-
-
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [timerStatus, setTimer] = useState(false);
+
+  const incNumVisits = () => {
+    console.log("in incNumVisits");
+    const date = new Date();
+    fetch('http://localhost:3001/incNumVisits', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        year: date.getFullYear(),
+        month: date.getMonth(),
+        day: date.getDate()
+      }),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(body => {
+        if (body.success) {
+          console.info(body.msg);
+        } else console.error(body.error);
+      })
+  }
+  
+  // runs twice because of these possible reasons: 
+  // https://stackoverflow.com/questions/60618844/react-hooks-useeffect-is-called-twice-even-if-an-empty-array-is-used-as-an-ar
+  useEffect(() => {
+    if (timerStatus==false) {
+      setTimer(true);
+      setTimeout(incNumVisits, 60000); // incNumVisits when user stayed for at least 1 minute
+    }
+  }, []);
+
   return (
     <div className="App">
       <Router>
