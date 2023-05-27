8fac3519c6a3a340324ebf3830263dfa68d473f9
ngarcia <ngarcia@up.edu.ph>
2023-05-27 17:19:18 +0800

    implementation of accommlist

---
+++ client/stals/src/pages/Home/AccommList.js
@@ -0,0 +1,16 @@
+import AccommCard from "./AccommCard";
+import "./Body.css"
+export default function AccommList({ isLoggedIn, bookmarkList, email, accommList }){
+
+
+    return(
+        <div className="body-group" 
+        id="inside">
+            {
+                accommList.map((accomm) => {
+                    return <AccommCard isLoggedIn={isLoggedIn} bookmarkList={bookmarkList} email={email} accomm={accomm} />
+                })
+            }
+        </div>
+    )
+}
