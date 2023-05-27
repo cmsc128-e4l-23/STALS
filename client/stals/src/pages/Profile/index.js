ce71f523f3778b0dd1aa6e1016504235a608f9d0
jfeching <jeching@up.edu.ph>
2023-05-27 19:02:22 +0800

    gets user data

--- client/stals/src/pages/Profile/index.js
+++ client/stals/src/pages/Profile/index.js
@@ -1,15 +1,41 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Header from "components/Header";
 import { UserPage } from "./user_profile";
-import { OwnerPage} from "./owner_profile";
+import { OwnerPage } from "./owner_profile";
 
 export default function Profile() {
   const data = []; // put the user data here
   // Example condition: isAdmin determines whether to show admin page or user page
-  const isOwner = true; //boolean to indicate which page will be rendered
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [usertype, setUserType] = useState('');
+
+  const [isLoggedIn, setLoggedIn] = useState(null);
+  const [isOwner, setIsOwner] = useState(false);
+  useEffect(() => {
+
+    fetch('http://localhost:3001/checkifloggedin', {
+      method: 'POST',
+      credentials: 'include'
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.isLoggedIn) {
+          setLoggedIn(data.isLoggedIn);
+          setName(data.name);
+          setEmail(data.email);
+          setUserType(data.usertype)
+          if (usertype === "Owner") setIsOwner(true)
+        }
+      })
+  }
+
+    , [isLoggedIn]);
   return (
     <div>
-      {isOwner?<OwnerPage/>:<UserPage/>}
+      {isOwner ? <OwnerPage /> : <UserPage email={email} />}
     </div>
   );
 }
