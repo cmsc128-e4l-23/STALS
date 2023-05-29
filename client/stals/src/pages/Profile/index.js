d0253a19813350e930999524eb2556f935cb962d
Xeth Soto <xasoto@up.edu.ph>
2023-05-29 20:34:53 +0800

    added missing bracket

--- client/stals/src/pages/Profile/index.js
+++ client/stals/src/pages/Profile/index.js
@@ -43,7 +43,7 @@ export default function Profile() {
       <>
       {userType === "Accommodation Owner" ? <OwnerPage user={email} /> : <UserPage user={email} />}
       </>
-
+    }
     </div>
   );
 }
