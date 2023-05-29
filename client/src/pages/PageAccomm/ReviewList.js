cea8cb318a141233382c5fd2fb37b14a7eab239b
RielMorales <rmmorales1@up.edu.ph>
2023-05-29 22:59:38 +0800

    description overflow button fixed

--- client/src/pages/PageAccomm/ReviewList.js
+++ client/src/pages/PageAccomm/ReviewList.js
@@ -1,14 +1,18 @@
 import React from "react";
+import "./ReviewList.css";
 import ReviewBody from "./ReviewBody";
 
 export default function ReviewList({ reviews }){
 
     return(
         <>
-            <h1>Reviews</h1>
+            <h1 className="review-title">Reviews</h1>
             {
-                reviews.length === 0 ? <h2>No Reviews</h2> :
-                reviews.map((review) => <div><ReviewBody reviewId={review}/></div>)
+                reviews.length === 0 ? <h2 className="no-reviews">No Reviews</h2> :
+                reviews.map((review) => 
+                    <div className="">
+                        <ReviewBody reviewId={review}/>
+                    </div>)
             }
         </>
     )
