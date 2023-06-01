7fdefe5a342d8ca705451cffd890b4e822ba5010
ngarcia <ngarcia@up.edu.ph>
2023-06-02 01:33:13 +0800

    add origins

--- server/app.js
+++ server/app.js
@@ -15,12 +15,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", 'https://e4l-frontend.onrender.com');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Origin,Accept,Content-Type");
     res.setHeader("Access-Control-Allow-Credentials","true");
     next();
 });
+app.use(cors({ origin: ["http://localhost:3000", "https://e4l-frontend.onrender.com"], credentials: true}))
 
 app.use(cookieParser());
 app.use(routes);
