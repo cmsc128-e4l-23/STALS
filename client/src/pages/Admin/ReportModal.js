1f00bb62a542ffac800c0397e5b5a65b74509e49
Marlo Mancenido <mumancenido@up.edu.ph>
2023-06-01 22:49:54 +0800

    Fixed Report Modal display values

--- client/src/pages/Admin/ReportModal.js
+++ client/src/pages/Admin/ReportModal.js
@@ -4,7 +4,7 @@ import "../ListAccomm/Modal.css";
 export default function ReportModal({ setModalOpen, report }) {
     return (
         <div className="modalBackground" style={{left:0}}>
-            <div className="modalContainer" style={{width:'unset', height:'unset', 'max-width': '80vw', 'min-height': '300px', 'max-height': '80vh', 'margin-top': '100px', 'min-width': '50vw'}}>
+            <div className="modalContainer" style={{width:'unset', height:'unset', 'max-width': '80vw', 'min-height': '130px', 'max-height': '80vh', 'margin-top': '100px', 'min-width': '30vw'}}>
                 <div className="titleCloseBtn">
                     <button
                         onClick={() => {
@@ -14,9 +14,10 @@ export default function ReportModal({ setModalOpen, report }) {
                         X
                     </button>
                 </div>
-                <div className="title">
-                    <h3>{report.content}</h3>
-                </div>
+                <span className="report-content"><b>Report Classification:</b> {report.classification}</span>
+                <span className="report-content"><b>Status: </b> {report.status}</span>
+                <br/>
+                <span className="report-content" style={{fontSize:'20px', marginTop:'15px', 'overflow':'scroll', marginBottom:'15px'}}><b>Content:</b> {report.content}</span>
             </div>
         </div>
     );
