8d3ad1cb8b7111ad7f2e6d095cb04431fadcadb3
ngarcia <ngarcia@up.edu.ph>
2023-06-02 01:25:19 +0800

    a

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://e4l-frontend.onrender.com' ");
+    res.setHeader("Access-Control-Allow-Origin", "https://e4l-frontend.onrender.com");
     res.setHeader("Access-Control-Allow-Methods", "POST");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Origin,Accept,Content-Type");
     res.setHeader("Access-Control-Allow-Credentials","true");
